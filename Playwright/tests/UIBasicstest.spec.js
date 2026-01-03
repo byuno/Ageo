af35a09bfff24f18a41fa0b28174003a62d08f11
Bobby Uno <robert.y.uno@gmail.com>
2026-01-02 19:14:25 -0800

    updated with variables

--- Playwright/tests/UIBasicstest.spec.js
+++ Playwright/tests/UIBasicstest.spec.js
@@ -1,15 +1,23 @@
 const { test, expect } = require('@playwright/test');
+const { sign } = require('node:crypto');
 
 test.only('Browswer Context First playwright test', async ({ browser, page })=> 
 {
+    const userName = page.locator('#username');
+    const signIn = page.locator('#signInBtn');
+
     // const context = await browser.newContext(); // new browser
     // const page = await context.newPage(); // new page in browser
     await page.goto('https://rahulshettyacademy.com/loginpagePractise/')
-    await page.locator('#username').fill('rahulshetty')
+    await userName.fill('rahulshetty')
     await page.locator('[type="password"]').fill('learning')
-    await page.locator('#signInBtn').click()
+    await signIn.click();
     console.log(await page.locator("[style*='block']").textContent());
     await expect(page.locator("[style*='block']")).toContainText('Incorrect username/password.')
+    //
+    await userName.fill('');
+    await userName.fill('rahulshettyacademy');
+    await signIn.click();
 });
 
 //default way
