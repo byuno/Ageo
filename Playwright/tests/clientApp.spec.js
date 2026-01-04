c88217bd6d191daee01a7abc2d2bf737c7236853
Bobby Uno <robert.y.uno@gmail.com>
2026-01-03 16:51:06 -0800

    waitForLoadState added

--- Playwright/tests/clientApp.spec.js
+++ Playwright/tests/clientApp.spec.js
@@ -9,6 +9,11 @@ test('Page First playwright test', async ({ page })=>
     await page.locator('#userEmail').fill('kusotare@hotmail.com');
     await page.locator('#userEmail').fill('Pass1234');
     await page.locator('#login').click()
+    await page.waitForLoadState('networkidle');
+
+    const titles = await page.locator(".card-body b").allTextContents(); //get all text
+    
+    console.log(titles);
 
 
 
