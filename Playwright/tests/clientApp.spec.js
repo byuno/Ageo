52679eeced8464976154ffe188de6a9236b71354
Bobby Uno <robert.y.uno@gmail.com>
2026-01-04 20:57:47 -0800

    minor edits

--- Playwright/tests/clientApp.spec.js
+++ Playwright/tests/clientApp.spec.js
@@ -7,13 +7,16 @@ test('Page First playwright test', async ({ page })=>
 {
     await page.goto('https://rahulshettyacademy.com/client');
     await page.locator('#userEmail').fill('kusotare@hotmail.com');
-    await page.locator('#userEmail').fill('Pass1234');
+    await page.locator('#userPassword').fill('Pass1234');
     await page.locator('#login').click()
-    await page.waitForLoadState('networkidle');
+    //await page.waitForLoadState('networkidle'); //flakey
+    await page.locator('.card-body b').first().waitFor();
 
     const titles = await page.locator(".card-body b").allTextContents(); //get all text
     
     console.log(titles);
+
+
 
 
 
